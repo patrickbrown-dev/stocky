6c0266b1dc06f82f1ab0399bd06c8d880c2b5eb7
Patrick Brown <patrick.arthur.brown@outlook.com>
2021-11-19 10:20:41 -0800

    Make output pretty (#5)
    * Bump deployment to 0.0.4
    
    * Improve output clarity and use finnhub library

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import { Client, Intents } from 'discord.js';
-import  dotenv from'dotenv';
-import got from 'got';
+import { Client, Intents, MessageEmbed } from 'discord.js';
+import  dotenv from 'dotenv';
+import finnhub from 'finnhub';
 
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const finnhub_api_key = process.env.FINNHUB_API_KEY;
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.FINNHUB_API_KEY;;
+const finnhubClient = new finnhub.DefaultApi()
+
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -15,21 +18,26 @@ client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	if (interaction.commandName === 'quote') {
-		const ticker = interaction.options.getString('ticker');
+		const ticker = interaction.options.getString('ticker').toUpperCase();
 
 		await interaction.deferReply()
-		await got.get(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${finnhub_api_key}`)
-			.json()
-			.then(json => interaction.editReply(`\`\`\`
-${ticker}
-	Current:    $${json["c"]}
-	Change:     $${json["d"]}
-	% Change:    ${json["dp"]}%
-	High:       $${json["h"]}
-	Low:        $${json["l"]}
-	Open:       $${json["o"]}
-	Prev Close: $${json["pc"]}
-\`\`\``));
+		let i = interaction;
+		finnhubClient.quote(ticker, (_e, data, _r) =>{
+			const embed = new MessageEmbed()
+				.setColor('#3ba55c')
+				.setTitle(`${ticker} Quote`)
+				.setDescription(`Here is how ${ticker} has been performing today.`)
+				.addFields(
+					{ name: 'Current', value: `$${data["c"]}`, inline: true },
+					{ name: 'Change', value: `$${data["d"]}`, inline: true },
+					{ name: 'Percent Change', value: `${data["dp"]}%`, inline: true },
+					{ name: 'High', value: `$${data["h"]}`, inline: true },
+					{ name: 'Low', value: `$${data["l"]}`, inline: true },
+					{ name: 'Open', value: `$${data["o"]}`, inline: true },
+					{ name: 'Previous Close', value: `$${data["pc"]}`, inline: true },
+				);
+			i.editReply({embeds: [embed]});
+		});
 	}
 });
 
